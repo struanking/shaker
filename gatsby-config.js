b821179b00c38bf1e31ce0f3ff29943c34111cff
Struan King <struan.king@gmail.com>
2018-07-05 20:35:04 +0100

    Adda Styled components to config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ module.exports = {
     //     pathToConfigModule: `src/utils/typography`,
     //   },
     // },
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-netlify` // put last in the array
   ]
 };
