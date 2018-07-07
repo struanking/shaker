8458ccaf137b13d85506f2cf122daf75d1fced7a
Struan King <struan.king@gmail.com>
2018-07-07 23:02:50 +0100

    Sets up cypress

---
+++ cypress/integration/homepage.spec.js
@@ -0,0 +1,17 @@
+/// <reference types="Cypress" />
+
+describe('Homepage', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('successfully loads', function() {
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/')
+    })
+  })
+
+  it('h1 text = "Shaker"', function() {
+    cy.get('h1').contains('Shaker')
+  })
+})
